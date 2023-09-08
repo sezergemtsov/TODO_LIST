12e188bc078b8be7343b1a97fbfc53b8a64ff76b
sezergemtsov <sezergemtsov@gmail.com>
2023-09-08 12:42:49 +0300

    select change color now

--- addTask.js
+++ addTask.js
@@ -5,6 +5,7 @@ function createTableElement(value, id) {
     var myTable = document.getElementById(id);
 
     var newLine = document.createElement("tr");
+    newLine.id = Math.round(Math.random()*1000, 3)*1000 + Math.round(Math.random()*1000, 3);
 
     var firstField = document.createElement("td");
     firstField.innerHTML = myTask;
@@ -13,14 +14,19 @@ function createTableElement(value, id) {
 
     var selection = document.createElement("select");
 
+    console.log(selection.id);
+
     var option1 = document.createElement("option");
     option1.innerHTML = "Waiting";
+    option1.value = "1";
 
     var option2 = document.createElement("option");
     option2.innerHTML = "In Process";
+    option2.value = "2";
 
     var option3 = document.createElement("option");
     option3.innerHTML = "Done";
+    option3.value = "3";
 
     selection.appendChild(option1);
     selection.appendChild(option2);
@@ -33,4 +39,30 @@ function createTableElement(value, id) {
 
     myTable.appendChild(newLine);
 
+    selection.addEventListener('change', () => changeColor(selection));
+
+}
+
+function changeColor(element){
+    var selection = element;
+    var column = element.parentNode;
+    var firstColumn = element.parentNode.parentNode.childNodes[0];
+    selection.classList.remove("line_wait", "line_done", "line_progress");
+    column.classList.remove("line_wait", "line_done", "line_progress");
+    firstColumn.classList.remove("line_wait", "line_done", "line_progress");
+    var option = selection.value;
+    console.log(option);
+    if(option==='1'){
+        selection.classList.add("line_wait");
+        column.classList.add("line_wait");
+        firstColumn.classList.add("line_wait");
+    } else if(option==='2') {
+        selection.classList.add("line_progress");
+        column.classList.add("line_progress");
+        firstColumn.classList.add("line_progress");
+    } else if(option==='3'){
+        selection.classList.add("line_done");
+        column.classList.add("line_done");
+        firstColumn.classList.add("line_done");
+    }
 }
